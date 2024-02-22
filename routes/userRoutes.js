d1658505e20d169f4d15ada5d0d43c77ba225112
Vedang Utekar <utekar.vedang2002@gmail.com>
2024-02-22 17:44:29 +0530

    add setavatar

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const { register } = require('../controllers/usersController');
 const { login } = require('../controllers/usersController');
+const { setAvatar } = require('../controllers/usersController');
 const router = require('express').Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/setAvatar/:id', setAvatar);
 
 module.exports = router;
