5302da8ecb582f7fed664da9aef92bacd3bf8b96
Vedang Utekar <utekar.vedang2002@gmail.com>
2024-02-26 01:45:35 +0530

    v1

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -64,3 +64,17 @@ module.exports.setAvatar = async (req, res, next) => {
         next(err);
     }
 }
+
+module.exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find({_id:{$ne: req.params.id}}).select([
+            "email",
+            "username",
+            "avatarImage"
+        ]);
+        return res.json(users);
+    }
+    catch(err){
+        next(err);
+    }
+}
--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
+const messageRoute = require('./routes/messagesRoute');
 
 const app = express();
+const socket = require('socket.io');
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoute);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('DB Connected!');
@@ -19,4 +22,27 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is running on port: ${process.env.PORT}`);
+})
+
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true
+    }
+})
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+        onlineUsers.set(userId, socket.id);
+    })
+
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("msg-receive", data.message);
+        }
+    })
 })
