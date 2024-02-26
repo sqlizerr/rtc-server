cd999075795a574f546ab450a894dcf0f790f727
Vedang Utekar <utekar.vedang2002@gmail.com>
2024-02-26 10:12:36 +0530

    cors

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.use(express.json());
 
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messageRoute);
+app.use(cors(
+    {
+        origin: "*",
+        credentials: true
+    }
+));
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('DB Connected!');
@@ -26,7 +32,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         credentials: true
     }
 })
